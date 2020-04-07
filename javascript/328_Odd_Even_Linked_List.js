b2d219ad13c569586a3af0501dd7d34f7299ff31
Victor Raul Tobar <victor@victortobar.com>
2020-04-06 21:25:09 -0700

    Implemenet 328 Odd Even Linked List

---
+++ javascript/328_Odd_Even_Linked_List.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+  if (!head) return head;
+  
+  let odd = head;
+  let even = head.next;
+  let evenHead = head.next;
+  
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = even.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  
+  odd.next = evenHead;
+  
+  return head;
+};

e2ea1ad813850120ae29ded003b40514d304e078
Victor Raul Tobar <victor@victortobar.com>
2020-04-18 21:48:18 -0700

    1 Two Sum

--- javascript/1_Two_Sum.js
+++ javascript/1_Two_Sum.js
@@ -7,11 +7,10 @@ var twoSum = function(nums, target) {
   const complements = {};
   
   for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (complements[complement] !== undefined) {
+      return [i, complements[complement]];
+    }
     complements[nums[i]] = i;
-  }
-  
-  for (let i = 0; i < nums.length; i++) {
-    const [complement, index] = [target-nums[i], complements[target-nums[i]]];
-    if (complement !== undefined && index !== i) return [i, index];
   }
 };
